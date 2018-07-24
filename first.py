2bb3020a58efb7ab271960731d593eef2a528e46
saisreedhar <saisreedhar789@gmail.com>
2018-07-24 14:42:34 +0530

    Python Modules Of all

---
+++ first.py
@@ -0,0 +1,27 @@
+import os
+
+
+import subprocess
+
+import sys
+
+import turtle
+
+import Numpy
+
+import Scipy
+
+import beautifulSoup
+
+import re
+
+import Tkinter 
+
+from Tkinter import *
+
+import socket 
+
+
+import pygame
+
+import GUI
